aa75ce752789b858f3d788a7be125516fada7b4d
Caterina <danizio@outlook.it>
2020-10-29 16:41:08 +0100

    Aggiunti alert score

--- js/script.js
+++ js/script.js
@@ -7,9 +7,14 @@
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito
 
+// VARIABILI GLOBALI
+var numUser;
+var score = 0;
+var blackRandom = [];
+var arrayUser = [];
+
 // LOGICA
 // Creare una funzione per generare 16 numeri random (e unici) da 1 a 100 e salvarli in un array
-var score = 0;
 getNumRandom(100,1,16);
 
 // Creare un prompt per chiedere all'utente un numero da 1 a 100 + check vari
@@ -17,13 +22,12 @@ getNumRandom(100,1,16);
 var numUser = numberRequest(numUser, 84);
 
 
-
 // FUNZIONI
 
   // Generazione numeri random
 
 function getNumRandom(max, min, time){
-  blackRandom = [];
+  // blackRandom = [];
   var i = blackRandom;
 while(blackRandom.length < time){
     var n = Math.floor(Math.random() * max) + min;
@@ -38,36 +42,44 @@ console.log("I numeri bomba sono ", blackRandom);
 // Richiesta numero utente
 
 function numberRequest(y, time){
-var arrayUser = [];
+// var arrayUser = [];
 var i = arrayUser;
 while(arrayUser.length < time){
   var y = parseInt(prompt("Inserisci un numero da 1 a 100"));
   // Mettere come condizioni che il numero sia stato inserito solo una volta e non appartenga alla black list
-    if (arrayUser.includes(y) && blackRandom.indexOf(y) === -1) {
+    if (y <= 100 && arrayUser.includes(y) && blackRandom.indexOf(y) === -1) {
       console.log("Hai già inserito questo numero");
     }
-    if(arrayUser.indexOf(y) === -1 && blackRandom.indexOf(y) === -1) {
+    if(y <= 100 && arrayUser.indexOf(y) === -1 && blackRandom.indexOf(y) === -1  && score < time) {
     arrayUser.push(y);
     console.log("I numeri inseriti dall'utente sono ", arrayUser);
+
+
+    // Per ogni numero corretto viene attribuito un punteggio
     score++;
     console.log("Il punteggio è ", score);
+  }
+
+  if (y > 100) {
+    alert("Il numero è maggiore di 100. Riprova")
+  }
+
+  if (score < time) {
+    var y = parseInt(prompt("Inserisci un numero da 1 a 100"));
+  }
+
+  else if (score = time) {
+    alert("Complimenti HAI VINTO!");
   }
 
 
     // Verificare se il numero inserito si trova o no nella black list dei 16 numeri "bomba"
       // Se il numero è nella black list --> GAME OVER
     if (blackRandom.includes(y)) {
-      return console.log("Game over");
+      console.log("Game over");
+      return alert("Peccato, hai perso! Il tuo punteggio è " + score);
     }
-
-
 
     i++;
 }
 }
-
-// Per ogni numero corretto viene attribuito un punteggio
-
-
-
-// Comunicare a fine partita il punteggio ottenuto
