6912dcab5cbb650ae207fc07261b9223c5cba551
Caterina <danizio@outlook.it>
2020-11-02 19:08:41 +0100

    Refactoring

--- js/script.js
+++ js/script.js
@@ -9,14 +9,13 @@
 
 // VARIABILI GLOBALI
 var numUser;
-var score = 0;
 var blackRandom = [];
 var arrayUser = [];
 
 // LOGICA
 
   // Generazione numeri random
-  var i = blackRandom;
+  var i = 0;
   while(blackRandom.length < 16) {
     var n = Math.floor(Math.random() * 100) + 1;
     if(blackRandom.indexOf(n) === -1) {blackRandom.push(n);
@@ -28,41 +27,45 @@ console.log("I numeri bomba sono ", blackRandom);
 
 
 // Richiesta numero utente
-var i = arrayUser;
-while(arrayUser.length < 84){
+var minaTrovata = false;
+var i = 0;
+while(arrayUser.length < 84 && minaTrovata == false){
   var numeroUser = parseInt(prompt("Inserisci un numero da 1 a 100"));
   // Mettere come condizioni che il numero sia stato inserito solo una volta e non appartenga alla black list
     if (numeroUser <= 100 && arrayUser.includes(numeroUser) && blackRandom.indexOf(numeroUser) === -1) {
       alert("Hai già inserito questo numero");
     }
-    if(numeroUser <= 100 && arrayUser.indexOf(numeroUser) === -1 && blackRandom.indexOf(numeroUser) === -1  && score < 84) {
+    if(numeroUser <= 100 && arrayUser.indexOf(numeroUser) === -1 && blackRandom.indexOf(numeroUser) === -1) {
     arrayUser.push(numeroUser);
     // debug
     console.log("I numeri inseriti dall'utente sono ", arrayUser);
 
 
     // Per ogni numero corretto viene attribuito un punteggio
-    score++;
     // debug
-    console.log("Il punteggio è ", score);
+    console.log("Il punteggio è ", arrayUser.length);
   }
+
+  // Se il numero è maggiore di 100
 
   if (numeroUser > 100) {
     alert("Il numero è maggiore di 100. Riprova")
   }
 
-  if (score === 84) {
+  // Se il giocatore totalizza 84 punti
+  if (arrayUser.length === 84) {
     alert("Complimenti HAI VINTO!");
-    window.location.reload();
   }
 
-    // Verificare se il numero inserito si trova o no nella black list dei 16 numeri "bomba"
-      // Se il numero è nella black list --> GAME OVER
+
+  // Verificare se il numero inserito si trova o no nella black list dei 16 numeri "bomba"
+    // Se il numero è nella black list --> GAME OVER
     if (blackRandom.includes(numeroUser)) {
       console.log("Game over");
-      alert("Peccato, hai perso! Il tuo punteggio è " + score);
-      window.location.reload();
+      alert("Peccato, hai perso! Il tuo punteggio è " + arrayUser.length);
+      minaTrovata == true;
     }
+
   i++;
  }
 // }
